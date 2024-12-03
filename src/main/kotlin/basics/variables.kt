a7173423634c89e708f861bed21db1293db83bd5
ananya-gta <anannya.gta@gmail.com>
2024-12-03 10:31:33 +0530

    string manipulation and how to us emultiline

--- src/main/kotlin/basics/variables.kt
+++ src/main/kotlin/basics/variables.kt
@@ -9,4 +9,22 @@ fun main() {
     println(age)
     age = 24
     println(age)
+
+    val salary = 3000L
+    println(salary)
+
+    val course = "Kotlin Spring"
+    println("course: $course and the course length is ${course.length}")
+
+    // create a multiline
+    // in general
+    val multiline = "ABC \n DEF"
+    println("multiline in general:\n $multiline")
+
+    val multilineInKotlin = """
+        ABC
+        DEF
+    """.trimIndent()
+    println("multiline in Kotlin:\n $multilineInKotlin")
+
 }

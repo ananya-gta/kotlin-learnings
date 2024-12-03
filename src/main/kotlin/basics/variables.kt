bbe54e8060bc7cb8daad504175fd24257e128a3c
ananya-gta <anannya.gta@gmail.com>
2024-12-03 10:22:07 +0530

    use of val and var

--- src/main/kotlin/basics/variables.kt
+++ src/main/kotlin/basics/variables.kt
@@ -4,4 +4,9 @@ fun main() {
     // val declared variables are immutable
     val name : String = "Ananya"
     println(name)
+
+    var age = 23
+    println(age)
+    age = 24
+    println(age)
 }

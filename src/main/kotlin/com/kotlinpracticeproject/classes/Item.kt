02ec88aef1667962377bb0858baebfed63e5cb20
ananya-gta <anannya.gta@gmail.com>
2024-12-06 15:14:54 +0530

    secondary constructor

---
+++ src/main/kotlin/com/kotlinpracticeproject/classes/Item.kt
@@ -0,0 +1,19 @@
+package com.kotlinpracticeproject.classes
+
+class Item() {
+    var name: String = ""
+
+    // secondary constructor
+    constructor(_name: String) : this() {
+        this.name = _name
+    }
+    // this() invokes the primary constructor
+    // this.name = _name assigns the value of the _name parameter to the name property of the Item instance
+}
+
+fun main() {
+    val item = Item("iPhone")
+    println(item.name)
+    item.name = "iPhone 15"
+    println(item.name)
+}
--- src/main/kotlin/com/kotlinpracticeproject/classes/Person.kt
+++ src/main/kotlin/com/kotlinpracticeproject/classes/Person.kt
@@ -8,16 +8,23 @@ package com.kotlinpracticeproject.classes
 
 // Primary constructor
 class Person(val name: String = "", val age: Int = 0) {
+    var email : String = ""
+    constructor(_email: String, _name: String="", _age:Int=0) : this(_name, _age) {
+        this.email = _email
+    }
+
     fun action() {
         println("Person walks.")
     }
 }
-
 fun main() {
     // no need to use `new` keyword to create instance of a class
     // val person = Person()
     val person = Person("Alex")
-    val person1 = Person("Alex", 29)
+    // val person = Person("Alex", 29)
     person.action()
     println("Name is ${person.name} and age is ${person.age}")
+
+    val personUsingSecondaryConstructor = Person(_email="anu@gmail.com", "Alex", 7)
+    println("Name is ${personUsingSecondaryConstructor.name} and age is ${personUsingSecondaryConstructor.age} and email is ${personUsingSecondaryConstructor.email}")
 }

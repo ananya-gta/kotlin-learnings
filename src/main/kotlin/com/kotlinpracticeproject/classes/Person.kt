d8a9371ced2d65b5cf2f0ff755277df05d81cedb
ananya-gta <anannya.gta@gmail.com>
2024-12-06 14:45:29 +0530

    primary constructor

--- src/main/kotlin/com/kotlinpracticeproject/classes/Person.kt
+++ src/main/kotlin/com/kotlinpracticeproject/classes/Person.kt
@@ -1,6 +1,13 @@
 package com.kotlinpracticeproject.classes
 
-class Person {
+//class Person {
+//    fun action() {
+//        println("Person walks.")
+//    }
+//}
+
+// Primary constructor
+class Person(val name: String = "", val age: Int = 0) {
     fun action() {
         println("Person walks.")
     }
@@ -8,6 +15,9 @@ class Person {
 
 fun main() {
     // no need to use `new` keyword to create instance of a class
-    val person = Person()
+    // val person = Person()
+    val person = Person("Alex")
+    val person1 = Person("Alex", 29)
     person.action()
+    println("Name is ${person.name} and age is ${person.age}")
 }

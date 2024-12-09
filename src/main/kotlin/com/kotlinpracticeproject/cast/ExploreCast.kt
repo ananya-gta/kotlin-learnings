9df5088d5f1794974fc3c12debcca4da80a4461a
ananya-gta <anannya.gta@gmail.com>
2024-12-09 17:44:46 +0530

    type checking, casting and smart casting

---
+++ src/main/kotlin/com/kotlinpracticeproject/cast/ExploreCast.kt
@@ -0,0 +1,41 @@
+package com.kotlinpracticeproject.cast
+
+import com.kotlinpracticeproject.classes.Course
+
+fun main() {
+    val course = Course("Kotlin Practice", 1, "Ananya Gupta")
+
+    checkType(course)
+    checkType("COURSE")
+    castNumber(1.0)
+    // castNumber(1) // will get ClassCastException if you try to cast a value which is not compatible
+
+    // to convert
+    val number = 1
+    println(checkType(number.toDouble()))
+}
+
+fun checkType(type: Any) {
+    when(type) {
+        // is will check if it is equal to an instance of operator
+        is Course -> {
+           println(type.copy())
+        }
+        is Double -> {
+            println("is double")
+        }
+        is String -> {
+            // TYPE HERE IS HIGHLIGHTED IN GREEN BCZ ITS SMART CASTING IN KOTLIN
+            println(type.lowercase())
+        }
+    }
+}
+
+fun castNumber(any: Any) {
+    when (any) {
+        // as is used to cast a value to a certain type
+        any as Double -> {
+            println("value is Double")
+        }
+    }
+}

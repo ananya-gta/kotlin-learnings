452d8ca41a068caac6162a3c088d4600f155613c
ananya-gta <anannya.gta@gmail.com>
2024-12-09 17:48:33 +0530

    enum classes

--- src/main/kotlin/com/kotlinpracticeproject/classes/Course.kt
+++ src/main/kotlin/com/kotlinpracticeproject/classes/Course.kt
@@ -3,8 +3,13 @@ package com.kotlinpracticeproject.classes
 data class Course(
     val name: String,
     val id: Int,
-    val instructor: String
+    val instructor: String,
+    val courseCategory: CourseCategory = CourseCategory.DEVELOPMENT
 )
+
+enum class CourseCategory {
+    DEVELOPMENT, BUSINESS, DESIGN, MARKETING
+}
 
 fun main() {
     val course1 = Course("React Learning", 1,"Akshay Saini")
@@ -15,4 +20,7 @@ fun main() {
     println("Cloning course1")
     val course3 = course1.copy(instructor = "Ananya Gupta")
     println(course3)
+
+    val marketingCourse = Course("Marketing Course", 4, "Aditya", CourseCategory.MARKETING)
+    println(marketingCourse)
 }

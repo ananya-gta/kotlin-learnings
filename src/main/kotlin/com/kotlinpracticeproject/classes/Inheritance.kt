fd0e1ea478963ab2b4d9193628c0bc7bdabc56da
ananya-gta <anannya.gta@gmail.com>
2024-12-09 14:12:01 +0530

    use of companion keyword

--- src/main/kotlin/com/kotlinpracticeproject/classes/Inheritance.kt
+++ src/main/kotlin/com/kotlinpracticeproject/classes/Inheritance.kt
@@ -9,13 +9,21 @@ open class User(val name: String) {
 
 class Student(name: String) : User(name) {
     override var loggedIn: Boolean = false
+
     override fun login() {
         println("inside student login")
         super.login()
     }
+
+    companion object {
+        const val noOfCourses = 10
+        fun country() = "USA"
+    }
 }
 
+
 class Instructor(name: String) : User(name) {
+
     override fun login() {
         println("inside instructor login")
         super.login()
@@ -27,6 +35,10 @@ fun main() {
     println("name : ${student.name}")
     student.login()
     student.loggedIn = true
+    println("Logged in value is ${student.loggedIn}")
+
+    println("Country value is ${Student.country()}")
+    println("No. of courses is ${Student.noOfCourses}")
     val instructor = Instructor("Dilip")
     println("name : ${instructor.name}")
     instructor.login()

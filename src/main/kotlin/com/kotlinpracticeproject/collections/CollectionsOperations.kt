8d076dc8ffb7109bdc8ccafd7114b032993f4000
ananya-gta <anannya.gta@gmail.com>
2024-12-11 14:17:39 +0530

    map operations on collections

--- src/main/kotlin/com/kotlinpracticeproject/collections/CollectionsOperations.kt
+++ src/main/kotlin/com/kotlinpracticeproject/collections/CollectionsOperations.kt
@@ -8,11 +8,16 @@ import com.kotlinpracticeproject.dataset.courseList
 fun main() {
     val courseList = courseList()
 
-    val devPredicate = {c : Course -> c.category == CourseCategory.DEVELOPMENT}
-    val desPredicate = {c : Course -> c.category == CourseCategory.DESIGN}
+    val devPredicate = { c: Course -> c.category == CourseCategory.DEVELOPMENT }
+    val desPredicate = { c: Course -> c.category == CourseCategory.DESIGN }
 
+    println("------------------Exploring Filter Function--------------------")
     exploreFilter(courseList, devPredicate)
     exploreFilter(courseList, desPredicate)
+    println("------------------Exploring Map Function--------------------")
+    exploreMap(courseList)
+    println("------------------Exploring Map & Filter Function together--------------------")
+    exploreMapAndFilter(courseList, desPredicate)
 }
 
 fun exploreFilter(courseList: MutableList<Course>, predicate: (Course) -> Boolean) {
@@ -20,12 +25,26 @@ fun exploreFilter(courseList: MutableList<Course>, predicate: (Course) -> Boolea
     // it refers to the current element being processed in the list.
     courseList.
 //        filter { it.category == CourseCategory.DEVELOPMENT }
-            filter {predicate.invoke(it)}
+    filter { predicate.invoke(it) }
         .forEach {
-
-        println("developmentCourses : ${it.name}")
-    }
-}
+            println("developmentCourses : ${it.name}")
+        }
 
 // a predicate function is a function that takes an argument and returns a Boolean value (true or false).
 // It is often used to test a condition or evaluate a certain criterion on data, especially in higher-order functions like filter, any, all, and none in collections
+}
+
+// map function will convert from one type to another type
+fun exploreMap(courseList: MutableList<Course>) {
+    val allCourseNames = courseList.map { "${it.name} - ${it.category}" }.forEach { println(it) }
+
+    println(allCourseNames)
+}
+
+//combining map and filter functions
+fun exploreMapAndFilter(courseList: MutableList<Course>, predicate: (Course) -> Boolean) {
+    courseList
+        .filter(predicate)
+        .map { "${it.name} - ${it.category}" }
+        .forEach { println(it) }
+}

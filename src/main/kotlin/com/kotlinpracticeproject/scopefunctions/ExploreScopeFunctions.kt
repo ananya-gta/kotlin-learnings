8b054c26df3b9d9b2841878d32e593cf6f1b2f9d
ananya-gta <anannya.gta@gmail.com>
2024-12-16 18:54:17 +0530

    let scope function

--- src/main/kotlin/com/kotlinpracticeproject/scopefunctions/ExploreScopeFunctions.kt
+++ src/main/kotlin/com/kotlinpracticeproject/scopefunctions/ExploreScopeFunctions.kt
@@ -7,6 +7,8 @@ fun main() {
     exploreApply()
     exploreAlso()
     chainMultipleScopeFunctions()
+    exploreLet()
+    exploreWith()
 }
 
 fun exploreApply() {
@@ -42,4 +44,34 @@ fun chainMultipleScopeFunctions() {
         println("Course using both : $it")
     }
 //    println("course using also : $course")
+}
+
+fun exploreLet() {
+    val numbers = mutableListOf(1, 2, 3, 4, 5)
+    val result = numbers.map { it * 2 }.filter { it > 5 }
+    // normally we do like this
+    println("normally we do like this : $result")
+
+//    ----USING LET------
+    val sum = numbers
+        .map { it * 2 }
+        .filter { it > 5 }
+        .let {
+            println("Using let scope function: $it")
+            it.sum()
+        }
+    println(sum)
+
+    // example -2
+    var name : String? = null
+    var capitalize = name?.let {
+        println(it)
+        it.uppercase()
+    }
+    println(capitalize)
+
+}
+
+fun exploreWith() {
+
 }
